bbc886f8bcebd565837ea6b0d34d26cfbe169763
Leland Jansen <hello@lelandjansen.com>
2019-03-02 23:25:29 -0700

    Fallback version of rustfmt and clippy

--- check-style.sh
+++ check-style.sh
@@ -2,6 +2,12 @@
 STATUS=0
 sort --check CONTRIBUTORS || STATUS=1
 sort --check rustfmt.toml || STATUS=1
-cargo +nightly fmt --all -- --check || STATUS=1
-cargo +nightly clippy -- -D warnings || STATUS=1
+# Fall back to an older nightly if the current version doesn't have rustfmt and
+# clippy
+cargo +nightly fmt --all -- --check ||
+  cargo +nightly-2019-02-26 fmt --all -- --check ||
+  STATUS=1
+cargo +nightly clippy -- -D warnings ||
+  cargo +nightly-2019-02-26 clippy -- -D warnings ||
+  STATUS=1
 exit $STATUS
