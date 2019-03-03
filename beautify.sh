ffd286cc1348b2727f489e69ece1ac67663ce7e8
Leland Jansen <hello@lelandjansen.com>
2019-03-02 23:57:22 -0700

    Fallback version of rustfmt and clippy (#15)

--- beautify.sh
+++ beautify.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 sort CONTRIBUTORS --output=CONTRIBUTORS
 sort rustfmt.toml --output=rustfmt.toml
-cargo +nightly fmt --all
+cargo +nightly fmt --all ||
+  cargo +nightly-2019-02-26 fmt --all
